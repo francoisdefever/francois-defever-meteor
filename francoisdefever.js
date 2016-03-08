57bd04440a4156fd50b4c06a5a9489f444fde643
francoisdefever <fdefever.work@gmail.com>
2016-03-08 11:08:07 +0100

    add header video code

--- francoisdefever.js
+++ francoisdefever.js
@@ -1,18 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.header.helpers({
+    vbgOptions () { return {
+      id: '157352489', // for https://vimeo.com/channels/staffpicks/140850530
+      type: 'vimeo',
+      poster: '/public/Snapshots/PC-Typing.jpg', // An image inside your public folder
+      aspectRatio: 1.8,
+      autoSize: 1
+    }}
   });
 }
 
